bfde75e24f27e379ee1d517fd9501144a77e5d2d
Kenko Nakamura <kenkou.n@gmail.com>
2018-06-24 19:51:06 +0900

    Copy-Pasted JSON parser

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 
 use combine::error::{Consumed, ConsumedResult, ParseError, Tracked};
 use combine::lib::marker::PhantomData;
+use combine::StreamOnce::{Item, Position, Range};
 use combine::{Parser, Stream, StreamOnce};
 
 use combine::parser::char::{char, digit, spaces, string};
@@ -27,11 +28,7 @@ fn lex<P>(p: P) -> impl Parser<Input = P::Input, Output = P::Output>
 where
     P: Parser,
     P::Input: Stream<Item = char>,
-    <P::Input as StreamOnce>::Error: ParseError<
-        <P::Input as StreamOnce>::Item,
-        <P::Input as StreamOnce>::Range,
-        <P::Input as StreamOnce>::Position,
-    >,
+    <P::Input as StreamOnce>::Error: ParseError<Item, Range, Position>,
 {
     p.skip(spaces())
 }
